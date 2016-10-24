8cd3abac17692a20b4406803363431027b4cbeb9
juangod1 <juan.godfrid@gmail.com>
2016-10-24 15:05:04 -0300

    Assets

--- core/src/com/game/Controller.java
+++ core/src/com/game/Controller.java
@@ -4,24 +4,31 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Controller extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Sprite bad;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		bad = new Sprite(img);
+		bad.setPosition(100, 100);
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		bad.translateX(5);
+
+		Gdx.gl.glClearColor(1, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		bad.draw(batch);
 		batch.end();
 	}
 	
