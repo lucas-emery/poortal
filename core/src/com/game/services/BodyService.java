71579e4f5beceab914696528f5aca2f47611405e
Lucas Emery <lucas.emery@hotmail.com>
2016-10-25 23:11:30 -0300

    #3 BodyService - isDynamic

--- core/src/com/game/services/BodyService.java
+++ core/src/com/game/services/BodyService.java
@@ -2,11 +2,15 @@ package com.game.services;
 
 import com.game.models.LevelObject;
 
-/**
- * Created by lemery on 25/10/16.
- */
+
 public class BodyService {
     public static Boolean isDynamic(LevelObject.Type type){
-        return true;
+        switch(type) {
+            case CUBE:
+                return true;
+
+            default:
+                return false;
+        }
     }
 }
