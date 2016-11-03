576492bff06b0aefda803dde960de56f16910910
juangod1 <juan.godfrid@gmail.com>
2016-11-03 16:15:50 -0300

    door controller sin terminar

--- core/src/com/game/controllers/DoorController.java
+++ core/src/com/game/controllers/DoorController.java
@@ -4,4 +4,17 @@ package com.game.controllers;
  * Created by juan on 03/11/16.
  */
 public class DoorController {
+    private static boolean isOpen;
+
+    public void open(){
+        isOpen = true;
+    }
+
+    public void close(){
+        isOpen = false;
+    }
+
+    public boolean doorOpenState(){
+        return isOpen;
+    }
 }
