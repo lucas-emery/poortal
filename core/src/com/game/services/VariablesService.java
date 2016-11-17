0015ee557f16118bc0f4b105110a5ef470348b8e
juangod1 <juan.godfrid@gmail.com>
2016-11-16 22:01:39 -0300

    Level updates, theme placement, cube physics tweak

--- core/src/com/game/services/VariablesService.java
+++ core/src/com/game/services/VariablesService.java
@@ -6,7 +6,7 @@ package com.game.services;
 
 public class VariablesService {
 
-    public static boolean SHOW_FULLSCREEN = false; // Toggle with F
+    public static boolean SHOW_FULLSCREEN = false;
 
     public static boolean SHOW_FPS = true; // Toggle with G
 
